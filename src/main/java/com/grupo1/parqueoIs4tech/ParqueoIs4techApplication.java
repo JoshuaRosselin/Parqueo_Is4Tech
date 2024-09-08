eec34099678d3ca0d5150d4408588bfe841efc9b
carlosaltan18 <carlosaltan850@gmail.com>
2024-09-08 17:59:39 -0600

    Proyecto Inicial

---
+++ src/main/java/com/grupo1/parqueoIs4tech/ParqueoIs4techApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.parqueoIs4tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueoIs4techApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueoIs4techApplication.class, args);
+	}
+
+}
